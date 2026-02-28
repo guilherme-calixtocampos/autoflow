f47be1c3cf4c39623b2983f92e6703d744f78b43
Guilherme Calixto Campos <gcalixtocampos16@gmail.com>
2026-02-28 18:53:51 -0300

    cria campo de busca por cpf e cria modal

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,20 @@
-const btn = document.querySelector('#btnHamburguer')
+const btnHamburguer = document.querySelector('#btnHamburguer')
 const menu = document.querySelector('#menuHamburguer')
 
-btn.addEventListener('click', () => {
+btnHamburguer.addEventListener('click', () => {
     menu.classList.toggle('hidden')
 })
+
+const btnNovoCliente = document.getElementById("btnNovoCliente");
+const modal = document.getElementById("modalCliente");
+const fechar = document.getElementById("fecharModal");
+
+btnNovoCliente.addEventListener("click", () => {
+  modal.classList.remove("hidden");
+  modal.classList.add("flex");
+});
+
+fechar.addEventListener("click", () => {
+  modal.classList.add("hidden");
+  modal.classList.remove("flex");
+});
